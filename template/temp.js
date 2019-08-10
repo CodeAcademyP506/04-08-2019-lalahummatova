d10f9105de3cd88e88caea760485b2a763e459ed
lalahummatova <50095672+lalahummatova@users.noreply.github.com>
2019-08-10 11:51:33 +0300

    Add files via upload

---
+++ template/temp.js
@@ -0,0 +1,19 @@
+// function myFunction(event) {
+//     var y = window.scrollY;
+  
+//   }
+window.addEventListener("scroll",function(){
+    var myabout= $("#parker").offset();    
+      if(window.scrollY>myabout.top){
+        $("nav").
+      }
+    //   console.log(window.scrollY)
+  })
+
+  window.addEventListener("scroll",function(){
+    var myabout= $("#about").offset();    
+      if(window.scrollY>myabout.top){
+        
+      }
+    //   console.log(window.scrollY)
+  })
